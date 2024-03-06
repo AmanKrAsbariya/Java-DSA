4c06edf72cfa0e3211b01ee53043d94a8f84931c
Aman Kr Asbariya <aman.asbariya811308@gmail.com>
2024-03-06 12:24:54 +0530

    This is my second commit

---
+++ programs/src/loops/Example1.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		//print 1 to 5
+		for(int i = 1 ; i <= 5 ; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ programs/src/loops/Nestedloops.java
@@ -0,0 +1,21 @@
+package loops;
+
+public class Nestedloops {
+
+	public static void main(String[] args) {
+		
+		//syntax : for(){ for(){ } }; 
+		
+		for(int i = 1; i<=4; i = i+1) {
+			System.out.println("Outer loop Starts");
+			for(int j = 1; j<=4; j = j+1) {
+				System.out.println("inner loop");
+			}
+			
+			System.out.println("Outer loop Finish");
+			//System.out.println();
+		}
+
+	}
+
+}
