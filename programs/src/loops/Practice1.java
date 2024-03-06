2956c20a12e6bdead254bcd71372082b1ae39afd
Aman Kr Asbariya <aman.asbariya811308@gmail.com>
2024-03-06 13:49:34 +0530

    This is my 3rd commit

--- programs/src/loops/Practice1.java
+++ programs/src/loops/Practice1.java
@@ -5,10 +5,11 @@ public class Practice1 {
 	public static void main(String[] args) {
 		
 		//outer loop runs : 4 times , inner loop decrease by 1
-		//print   1 2 3 4
-		//        5 6 7
-		//        8 9
-		//        10
+		//print
+		// 1 2 3 4
+		// 5 6 7
+		// 8 9
+		// 10
 
 		int k = 1;
 		for(int i = 1; i < 5 ; i++) {
